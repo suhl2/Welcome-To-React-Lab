64eb07beee7e0bb874154d2150593301985c5686
Sarah <sarah.celiann@gmail.com>
2023-01-18 11:12:04 -0600

    lab done

--- src/index.js
+++ src/index.js
@@ -1,1 +1,23 @@
-// Your code here!
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { useState } from 'react';
+
+const Counter = () => {
+    const [count, setCount] = useState(0);
+    const increment = () => {
+        setCount(count + 1)
+}
+  return (
+    <div id='container'>
+      <div id='navbar'>
+        Counter.js
+      </div>
+      <div id='counter'>
+        <h1>{count}</h1>
+        <button onClick={increment}>Increment</button>
+      </div>
+    </div>
+  )
+}
+
+ReactDOM.render(<Counter />, document.getElementById('app'));
